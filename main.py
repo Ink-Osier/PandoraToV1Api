d6b70a507a5761eed739c42e9866172cd4a59c28
biek12 <1274180586@qq.com>
2024-01-04 21:59:04 +0800

    [fix]增加下载图片时的随机UA提升图片下载成功率

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ from PIL import Image
 from io import BytesIO
 from urllib.parse import urlparse, urlunparse
 import base64
+from fake_useragent import UserAgent
 
 # 读取配置文件
 def load_config(file_path):
@@ -62,6 +63,8 @@ CUSTOM_ARKOSE = CONFIG.get('custom_arkose_url', 'false').lower() == 'true'
 
 ARKOSE_URLS = CONFIG.get('arkose_urls', "")
 
+# 创建FakeUserAgent对象
+ua = UserAgent()
 
 # 设置日志级别
 log_level_dict = {
@@ -544,7 +547,10 @@ def send_text_prompt_and_get_response(messages, api_key, stream, model):
                         else:
                             # 处理普通的文件URL
                             try:
-                                file_response = requests.get(file_url)
+                                headers = {
+                                    'User-Agent': ua.random
+                                }
+                                file_response = requests.get(url=file_url, headers=headers)
                                 file_content = file_response.content
                                 mime_type = file_response.headers.get('Content-Type', '').split(';')[0].strip()
                             except Exception as e:
